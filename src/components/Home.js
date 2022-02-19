3adae651683c1bf9a33ae86bed465e6f585482c8
Jagbir <birchaudhary2000@gmail.com>
2022-02-19 15:44:31 +0930

    registrations added

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,11 +11,11 @@ import SlideImage8 from "../images/Slide-Image-8.jpg";
 
 import sponsors from "../images/major sponsors.png"
 
-import { Container, Row, Col, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import { Container, Row, Col, Card, ListGroup, ListGroupItem, Button } from 'react-bootstrap';
 
 export default function Home() {
   return (
-    <Container>
+    <Container fluid>
         <Row style={{paddingTop: "3px", paddingBottom:"3px"}}>
             <Col style={{paddingTop: "3px", paddingBottom:"3px"}}>
                 <Carousel variant="dark">
@@ -23,7 +23,6 @@ export default function Home() {
                         <img className="d-block w-100" src={SlideImage1} alt="First slide"/>
                         <Carousel.Caption>
                         <h1>Welcome to Darwin Hearts Football Club</h1>
-                        {/* <p>Darwin Hearts FC is non-profit sports organisation established in 2010 under the management of Nepalese Association of Northern Territory Inc., referred as NANT hereafter. It was a huge effort of football passionate Nepali youths residing in Darwin to socialise and harmonise with the sole sports activities. Therefore, in the beginning it was limited to a fun and fitness activity for us where many non-Nepali locals and foreigner friends joined the activities regularly. Along with the time everyone was getting better with their skills and they started believing in themselves to compete with similar other teams formed around and inter-state.</p> */}
                         </Carousel.Caption>
                         </Carousel.Item>
                         <Carousel.Item><img className="d-block w-100" src={SlideImage2} alt="Second slide"/></Carousel.Item>
@@ -33,18 +32,67 @@ export default function Home() {
                         <Carousel.Item><img className="d-block w-100" src={SlideImage6} alt="Sixth slide"/></Carousel.Item>
                         <Carousel.Item><img className="d-block w-100" src={SlideImage7} alt="Seventh slide"/></Carousel.Item>
                         <Carousel.Item><img className="d-block w-100" src={SlideImage8} alt="Eighth slide"/></Carousel.Item>
-                    {/* <Carousel.Item>
-                        <img
-                        className="d-block w-100"
-                        src={img1}
-                        alt="First slide"
-                        />
-                        <Carousel.Caption>
-                        <h5>First slide label</h5>
-                        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                        </Carousel.Caption>
-                    </Carousel.Item>  */}
                 </Carousel>
+            </Col>
+        </Row>
+        <Row style={{paddingTop: "3px", paddingBottom:"3px"}}><Col style={{paddingTop: "3px", paddingBottom:"3px", alignItems:"center"}}>REGISTRATIONS</Col></Row>
+        <Row style={{alignContent:"center"}}>
+            <Col>
+                <Card style={{ width: '14rem' }}>
+                    <Card.Body>
+                        <Card.Title>Become a Player</Card.Title>
+                        <Card.Text>
+                            Registration for a senior player.
+                        </Card.Text>
+                        <Button variant="primary" href="https://registration.playfootball.com.au/common/pages/reg/welcomeregplus.aspx?entityid=75620&id=81229">Seniors</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card style={{ width: '14rem' }}>
+                    <Card.Body>
+                        <Card.Title>Become a Player</Card.Title>
+                        <Card.Text>
+                            Registration for the age group 12-18 Years Old.
+                        </Card.Text>
+                        <Button variant="primary" href="https://registration.playfootball.com.au/common/pages/reg/welcomeregplus.aspx?entityid=75620&id=89310">Juniors</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card style={{ width: '14rem' }}>
+                    <Card.Body>
+                        <Card.Title>Become a Player</Card.Title>
+                        <Card.Text>
+                            Registration for MiniRoos U6-U11.
+                        </Card.Text>
+                        <Button variant="primary" href="https://registration.playfootball.com.au/common/pages/reg/welcomeregplus.aspx?entityid=75620&id=81227">Miniroos</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card style={{ width: '14rem' }}>
+                    <Card.Body>
+                        <Card.Title>Become a Coach</Card.Title>
+                        <Card.Text>
+                            Registration for a Coach. <br/><br/>
+                        </Card.Text>
+                        <Button variant="primary"  href="https://registration.playfootball.com.au/common/pages/reg/welcomeregplus.aspx?entityid=75620&id=81230
+">Coach</Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+            <Col>
+                <Card style={{ width: '14rem' }}>
+                    <Card.Body>
+                        <Card.Title>Become a Volunteer</Card.Title>
+                        <Card.Text>
+                            Registration for a volunteer.
+                        </Card.Text>
+                        <Button variant="primary" href="https://registration.playfootball.com.au/common/pages/reg/welcomeregplus.aspx?entityid=75620&id=81230
+">Volunteer</Button>
+                    </Card.Body>
+                </Card>
             </Col>
         </Row>
         <Row style={{paddingTop: "3px", paddingBottom:"3px"}}><Col style={{paddingTop: "3px", paddingBottom:"3px"}}>SPONSORS</Col></Row>
